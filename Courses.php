70f3e05d90467e00ec209dbb2c87fa1901799292
Jeannick94 <jeannickumba94@gmail.com>
2021-01-06 03:51:00 +0800

    Umba Ilunga Jeannick 202025800049 & Mvondo Jean Claude Wilfried 202025800038 Fixed bug No=74 unable to update attachment for posted assignments.

--- Courses.php
+++ Courses.php
@@ -127,8 +127,18 @@ New Date/Time <br><input type="date" name="date" required=""> <input type="time"
                     $type  = $_POST["type"];
                     $Deadline = $deadlinedate." ".$deadlinetime;
                     $date =  date("Y-m-d H:i");
+                     $targetfile="";
+                    $targetfile2="";
+                    $targetfile3="";
+                    $targetfile4="";
+                    if($_FILES['attachment1']['name']!=""){   $targetfile  = "/".$title."/".$_FILES['attachment1']['name']; }
+                    if($_FILES['attachment2']['name']!=""){   $targetfile2 = "/".$title."/".$_FILES['attachment2']['name']; }
+                    if($_FILES['attachment3']['name']!=""){   $targetfile3 = "/".$title."/".$_FILES['attachment3']['name']; }
+                    if($_FILES['attachment4']['name']!=""){   $targetfile4 = "/".$title."/".$_FILES['attachment4']['name']; }
     
-                    $sql = "UPDATE `lab_reports_table` SET `Deadline` = ('" . $Deadline . "'), `Instructions` = ('" . $instructions . "'), `Title` = ('" . $title . "'), `Marks` = ('" . $marks . "'), `Type` = ('" . $type . "') WHERE `lab_reports_table`.`Lab_Report_ID` = '$getid'";
+                    $sql = "UPDATE `lab_reports_table` SET `Deadline` = ('" . $Deadline . "'), `Instructions` = ('" . $instructions . "'), `Title` = ('" . $title . "'),
+                             `Attachment_link_1` = ('" . $targetfile . "'), `Attachment_link_2` = ('" . $targetfile2 . "'), `Attachment_link_3` = ('" . $targetfile3 . "'),
+                              `Attachment_link_4` = ('" . $targetfile4 . "'),  `Marks` = ('" . $marks . "'), `Type` = ('" . $type . "') WHERE `lab_reports_table`.`Lab_Report_ID` = '$getid'";
                     if ($con->query($sql) === TRUE) {
                         $_SESSION["info_Updated"]="Assignment information updated successfully.";
        
@@ -148,7 +158,7 @@ New Date/Time <br><input type="date" name="date" required=""> <input type="time"
     
                     echo "  <h3><a href='Courses.php?course=".$url."'> Editing Lab Assignment </a></h3>";
                     ?> 
-                    <form method='post'   enctype='multipart/form-data' action=''>
+                    <form method='post'   enctype='multipart/form-data' action='Script.php'>
                     <input type='hidden' name='frm_uploadlab' value='true' required=''/>
                     <input type='hidden' name='course_id' value='<?php echo "$id" ?>' required=''/>
                     <input type='hidden' name='url' value='<?php echo ".$course_url." ?>' required=''/>
@@ -166,17 +176,17 @@ New Date/Time <br><input type="date" name="date" required=""> <input type="time"
                     Marks
                         <input type='text'  name='marks' placeholder='Marks' class='form-control' required='' value="<?php echo isset($_GET['act']) && $_GET['act']=="edit" ? $Marks : ""; ?>">
                     Attachment 1
-                        <input type='file'  name='attachment1' placeholder='Attachment 1' class='form-control'>
+                        <input type='file'  name='attachment1' placeholder='Attachment 1' class='form-control'value="<?php echo isset($_GET['act']) && $_GET['act']=="edit" ? $targetfile : ""; ?>">
 
                     Attachment 2
-                        <input type='file' name='attachment2' placeholder='Attachment 1' class='form-control'>
+                        <input type='file' name='attachment2' placeholder='Attachment 1' class='form-control'value="<?php echo isset($_GET['act']) && $_GET['act']=="edit" ? $targetfile2 : ""; ?>">
 
                     Attachment 3
-                        <input type='file'  name='attachment3' placeholder='Attachment 1' class='form-control' >
+                        <input type='file'  name='attachment3' placeholder='Attachment 1' class='form-control' value="<?php echo isset($_GET['act']) && $_GET['act']=="edit" ? $targetfile3 : ""; ?>">
 
 
                     Attachment 4
-                        <input type='file'  name='attachment4' placeholder='Attachment 4' class='form-control' >
+                        <input type='file'  name='attachment4' placeholder='Attachment 4' class='form-control' value="<?php echo isset($_GET['act']) && $_GET['act']=="edit" ? $targetfile4 : ""; ?>">
                     <br>
 
 <?php
